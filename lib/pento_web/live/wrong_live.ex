00fd91669c2ce40c86d126bfd86433dc75faf293
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2021-04-04 10:43:15 -0300

    Guess random number

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,24 +2,22 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    # IO.inspect(socket, label: "mount")
-
     if connected?(socket) do
       :timer.send_interval(1000, self(), :tick)
     end
 
-    {:ok,
-     assign(socket, score: 0, message: "Guess a number", time: time(), number: random_number())}
+    socket = assign_state(socket)
+    {:ok, socket}
   end
 
   def render(assigns) do
     ~L"""
+    <h2> It's: <%= @time %> </h2>
     <h1> Your Score: <%= @score %> </h1>
-    <%= @number %>
     <h2>
       <%= @message %>
-      It's <%= @time %>
     </h2>
+
     <h2>
     <%= for n <- 1..10 do %>
       <a href="#" phx-click="guess" phx-value-number="<%= n %>">
@@ -32,21 +30,40 @@ defmodule PentoWeb.WrongLive do
 
   def time() do
     DateTime.utc_now()
-    |> to_string()
+    |> Calendar.strftime("%c", preferred_datetime: "%H:%M:%S - %B %-d, %Y")
   end
 
-  def random_number(), do: Enum.random(1..10)
+  def random_number(), do: Enum.random(1..10) |> to_string()
 
   def handle_event("guess", %{"number" => guess} = data, socket) do
     IO.inspect(data)
-
-    message = "Your guess: #{guess} Wrong. Guess again."
-    score = socket.assigns.score - 1
-    {:noreply, assign(socket, message: message, score: score, time: time())}
+    check_guess(guess, socket)
   end
 
   def handle_info(:tick, socket) do
     socket = assign(socket, time: time())
     {:noreply, socket}
+  end
+
+  defp assign_state(socket) do
+    socket
+    |> assign(
+      score: 0,
+      message: "Guess a number",
+      time: time(),
+      number: random_number()
+    )
+  end
+
+  defp check_guess(guess, socket) do
+    if guess == socket.assigns.number do
+      message = "Your guess: #{guess} Right. Guess again."
+      score = socket.assigns.score + 1
+      {:noreply, assign(socket, message: message, score: score, number: random_number())}
+    else
+      message = "Your guess: #{guess} Wrong. Guess again."
+      score = socket.assigns.score - 1
+      {:noreply, assign(socket, message: message, score: score)}
+    end
   end
 end
